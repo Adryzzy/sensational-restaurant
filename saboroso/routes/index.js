c218adfdd39a1ad47b2cbdc5f63fcc4fb0a76dbb
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-02-28 10:49:11 -0300

    refactor: organize HEADER using EJS include

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -26,19 +26,35 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/contacts', function(req, res, next) {
-  res.render('contacts', {title: 'Contact Us'});
+  res.render('contacts', {
+    title: 'Contact Us',
+    background: 'images/img_bg_3.jpg',
+    h1: 'diga oi'
+  });
 });
 
 router.get('/menus', function(req, res, next) {
-  res.render('menus', {title: 'Our Menus'});
+  res.render('menus', {
+    title: 'Our Menus',
+     background: 'images/img_bg_1.jpg',
+    h1: 'saboreie nosso main menu'
+  });
 });
 
 router.get('/reservations', function(req, res, next) {
-  res.render('reservations', {title: 'Make a Reservation'});
+  res.render('reservations', {
+    title: 'Make a Reservation',
+     background: 'images/img_bg_2.jpg',
+    h1: 'reserve sua mesa main'
+  });
 });
 
 router.get('/services', function(req, res, next) {
-  res.render('services', {title: 'Our Services'});
+  res.render('services', {
+    title: 'Our Services',
+     background: 'images/img_bg_1.jpg',
+    h1: 'q prazer obg'
+  });
 });
 
 module.exports = router;
