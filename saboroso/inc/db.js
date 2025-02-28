b05ec6f8e0bb7389457659b90fcc2181142488f0
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-02-28 09:33:37 -0300

    feat: connect Node.js to MySQL database

---
+++ saboroso/inc/db.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2');
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'Alex.',
+    database: 'saboroso',
+    password: 'adryan1/6'
+  });
+
+  module.exports = connection;
+  
