eb39676d801e92f28ffeed6ef83fdbe98e5701bc
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-03-07 12:36:44 -0300

    feat: add reservation management with READ functionality

--- saboroso/inc/reservations.js
+++ saboroso/inc/reservations.js
@@ -1,6 +1,19 @@
 var conn = require('./../inc/db');
 
 module.exports ={
+    getReservations(){
+        return new Promise((resolve, reject) => {
+            conn.query(`
+                SELECT * FROM tb_reservations ORDER BY date DESC
+            `, (err, results) => {
+                if(err){
+                    reject(err);
+                } 
+                    resolve(results);
+                
+            });
+        });
+    },
 
     render(req, res, error, success){
         res.render('reservations', {
