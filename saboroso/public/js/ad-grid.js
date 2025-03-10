e7e3e1c381558dd688d8620add6569a622fa843c
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-03-10 11:40:22 -0300

    feat: add user creation in admin panel

--- saboroso/public/js/ad-grid.js
+++ saboroso/public/js/ad-grid.js
@@ -44,7 +44,10 @@ class AdGrid {
             formUpdate: '#modal-update form',
             btnUpdate: '.btn-update',
             btnDelete: '.btn-delete',
-           
+            onUpdateLoad:(form,name,data) =>{
+              let input = form.querySelector('[name='+name+']');
+              if(input)  input.value = data[name];
+            }
 
         },configs) ;
 
